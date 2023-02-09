9b57f0cc289cfba0315e0336bc3f92ba6c624124
fjrjdev <thedromemafia@gmail.com>
2023-02-09 19:30:45 -0300

    feat: create link view

---
+++ links/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+
+class IsAdminOrUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.is_authenticated:
+            return request.user.is_superuser or request.user.is_active
