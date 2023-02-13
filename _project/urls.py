c9173cf8ae3a9a5b34d7a7f8b5339c10d324442a
fjrjdev <thedromemafia@gmail.com>
2023-02-13 17:21:21 -0300

    feat: webcrawler created

--- _project/urls.py
+++ _project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("links.urls")),
+    path("api/crawler/", include("webcrawler.urls")),
 ]
