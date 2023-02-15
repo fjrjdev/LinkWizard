6d98883ed42eb45307242e835bb0c982cffc3399
fjrjdev <thedromemafia@gmail.com>
2023-02-15 18:16:30 -0300

    refactor: web crawler redone

--- webcrawler/crawler.py
+++ webcrawler/crawler.py
@@ -1,20 +1,78 @@
-import requests
-from bs4 import BeautifulSoup
+from scrapy.crawler import CrawlerProcess
+from scrapy.spiders import Rule, Spider
+from scrapy.linkextractors import LinkExtractor
+import scrapy
+from urllib.parse import urljoin
+from scrapy.signalmanager import dispatcher
+from scrapy import signals
+
+import subprocess
 
 
-def crawler(url="https://devgo.com.br"):
-    response = requests.get(url)
-    url_list = []
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "html.parser")
-        h1_tags = soup.find_all("h1")
-        for h1_tag in h1_tags:
-            a_tag = h1_tag.find("a")
-            href = a_tag["href"]
-            text = a_tag.text
-            if href and href.startswith("/"):
-                href = url + href
-            if href != url + "/":
-                url_list.append({"url": href, "label": text})
+class DevgoSpider(Spider):
+    name = "devgo_spider"
 
-    return url_list
+    allowed_domains = ["devgo.com.br"]
+    start_urls = ["https://devgo.com.br/"]
+
+    def __init__(self, *args, **kwargs):
+        super(DevgoSpider, self).__init__(*args, **kwargs)
+        self.visited_urls = set()
+
+    @staticmethod
+    def is_valid_url(url):
+        if not url.startswith("http"):
+            return False
+        exclude = ["discord.gg", "hashnode.com"]
+        for e in exclude:
+            if e in url:
+                return False
+        if url is None:
+            return False
+        return True
+
+    def parse(self, response):
+        links = response.xpath("//a")
+        for link in links:
+            url = link.xpath("@href").get()
+            label = link.xpath("text()").get()
+
+            if not url:
+                continue
+
+            url = response.urljoin(url)
+
+            if label is None:
+                continue
+
+            if not self.is_valid_url(url):
+                continue
+
+            if url in self.visited_urls:
+                continue
+
+            self.visited_urls.add(url)
+
+            yield {"url": url, "label": label}
+
+
+def iniciar_crawler(nome_do_spider):
+    #     # chama o comando scrapy crawl nome_do_spider no terminal
+    subprocess.run(["scrapy", "runspider", nome_do_spider])
+
+
+# def spider_results():
+#     results = []
+
+#     def crawler_results(signal, sender, item, response, spider):
+#         results.append(item)
+
+#     dispatcher.connect(crawler_results, signal=signals.item_scraped)
+
+#     process = CrawlerProcess()
+#     process.crawl(DevgoSpider)
+#     process.start()
+#     return results
+
+
+# process.start()
--- webcrawler/views.py
+++ webcrawler/views.py
@@ -3,12 +3,38 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
 import uuid
+import django
 
-from .crawler import crawler
+from .crawler import DevgoSpider, iniciar_crawler
 
 from links.models import Link
 from links.permissions import IsAdminOwnerLink
 from links.serializers import LinkSerializer
+
+from concurrent.futures import ProcessPoolExecutor
+from scrapy.crawler import CrawlerRunner, CrawlerProcess
+from scrapy import signals
+from scrapy.utils.log import configure_logging
+from twisted.internet import reactor, defer
+import multiprocessing
+
+
+def run_spider(spider):
+    items = []
+
+    def item_scraped(item, response, spider):
+        items.append(item)
+
+    configure_logging()
+    process = CrawlerProcess()
+    crawler = process.create_crawler(spider)
+    crawler.signals.connect(item_scraped, signal=signals.item_scraped)
+    process.crawl(crawler)
+    process.start()
+    return items
+
+
+user = None
 
 
 class CrawlerView(APIView):
@@ -16,27 +42,34 @@ class CrawlerView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request: Request) -> Response:
-        data = crawler()
-        links_created = []
+        global user
+        user = self.request.user
+        with ProcessPoolExecutor(
+            max_workers=4,
+            mp_context=multiprocessing.get_context("spawn"),
+            initializer=django.setup,
+        ) as executor:
+            future1 = executor.submit(run_spider, DevgoSpider)
+            future1.add_done_callback(process_data)
+        return Response(status=status.HTTP_202_ACCEPTED)
+
+
+def process_data(future):
+    global user
+    try:
+        data = future.result()
+
         for link in data:
             obj, created = Link.objects.get_or_create(
                 url=link.get("url"),
-                user=self.request.user,
+                user=user,
                 defaults={
                     "id": uuid.uuid4(),
                     "url": link.get("url"),
                     "label": link.get("label"),
-                    "user": self.request.user,
+                    "user": user,
                 },
             )
-            if created:
-                links_created.append(obj)
 
-        if links_created:
-            serializer = LinkSerializer(links_created, many=True)
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-        return Response(
-            data={"detail": "Links já foram coletados anteriormente"},
-            status=status.HTTP_200_OK,
-        )
+    except Exception as e:
+        print(e)
